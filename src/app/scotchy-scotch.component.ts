cc658e981ea84aa461a04c2bfc490e46b4960a3c
taramouse <support@optimized4u.org>
2016-05-20 12:07:35 +0100

    Generated about route. Command 'ng generate route about'

--- src/app/scotchy-scotch.component.ts
+++ src/app/scotchy-scotch.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './+about';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'scotchy-scotch-app',
   templateUrl: 'scotchy-scotch.component.html',
-  styleUrls: ['scotchy-scotch.component.css']
+  styleUrls: ['scotchy-scotch.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/about', component: AboutComponent}
+])
 export class ScotchyScotchAppComponent {
   title = 'scotchy-scotch works!';
 }
